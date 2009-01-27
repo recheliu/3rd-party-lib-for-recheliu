f2d090e01af3a4b5a9562d735841f637c64c0b30
leeten <leeten@31c89a13-3246-848d-8fbe-81b5ef42b1a8>
2009-01-27 05:08:38 +0000

    [2009/01/26] 1. [CHANGE] Pause  the script at the end to verify the result.

--- bin/Install.bat
+++ bin/Install.bat
@@ -1,13 +1,20 @@
+@ECHO OFF
+
 REM
 REM	This script is used to copy the .dll in this folder to the system folder.
 REM
-
-@ECHO OFF
 
 ECHO Copy the *.dlls to %windir%\system.
 
 COPY *.dll %windir%\system\
 
+PAUSE
+
 REM
 REM	$Log: not supported by cvs2svn $
+REM	Revision 1.1  2008/08/22 13:48:04  leeten
+REM	
+REM	[2008/08/22]
+REM	1. [FIRST TIME CHECKIN] This script is used to copy the dll in this folder to the system folder.
+REM	
 REM	
